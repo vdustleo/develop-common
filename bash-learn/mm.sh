3de0060874e7839e1e9a816a95c8129076b1345a
vdust.leo <vdust.leo@163.com>
2017-12-27 21:21:15 +0800

    Add python-learn code, and some bash learn-code.

---
+++ bash-learn/mm.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+main()
+{
+    :
+}
+
+my_test(){
+
+:
+}
+
+ss=$PWD
+if [[ ${ss} == 1 ]]; then
+    echo "ss";
+fi
+
+if cat /dev/null; then
+    echo "sss"
+elif cat /dev/null; then
+    echo    "ok"
+else
+    :
+fi
+cat mm.$$ | awk -F"ss" | sed
+
+
+exit 1
